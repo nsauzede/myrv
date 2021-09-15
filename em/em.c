1d901d39b7a7a937faf849f2ad67bc49d1f4687c
Nicolas Sauzede <nsauzede@laposte.net>
2021-09-15 16:06:57 +0200

    Build esw with stdlib and allow different start_pc for em

--- em/em.c
+++ em/em.c
@@ -8,27 +8,43 @@
 uint32_t mem_start = 0;
 uint32_t mem_len = 0x4000;
 unsigned char *mem = 0;
-uint32_t rv_read32(uint32_t addr) {
+uint32_t rv_read32(uint32_t addr)
+{
   uint32_t val = 0;
-  if ((addr >= mem_start) && (addr + 4 <= mem_start + mem_len)) {
+  if ((addr >= mem_start) && (addr + 4 <= mem_start + mem_len))
+  {
     memcpy(&val, mem + addr - mem_start, 4);
   }
   return val;
 }
 
-int rv_write32(uint32_t addr, uint32_t val) {
-  if ((addr >= mem_start) && (addr + 4 <= mem_start + mem_len)) {
+int rv_write32(uint32_t addr, uint32_t val)
+{
+  if ((addr >= mem_start) && (addr + 4 <= mem_start + mem_len))
+  {
     memcpy(mem + addr - mem_start, &val, 4);
     return 0;
   }
   return 1;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+  uint32_t start_pc = 0;
+  uint32_t start_sp = 0x2000;
   char *fin = "em_esw.bin";
   int arg = 1;
-  if (arg < argc) {
+  if (arg < argc)
+  {
     fin = argv[arg++];
+    if (arg < argc)
+    {
+      sscanf(argv[arg++], "%" SCNx32, &start_pc);
+      if (arg < argc)
+      {
+        sscanf(argv[arg++], "%" SCNx32, &start_sp);
+      }
+    }
   }
   mem = calloc(mem_len, 1);
   FILE *in = fopen(fin, "rb");
@@ -37,12 +53,15 @@ int main(int argc, char *argv[]) {
 
   rv_ctx ctx;
   rv_init(&ctx, rv_read32, rv_write32);
-  ctx.sp = 0x2000;
+  ctx.sp = start_sp;
+  ctx.pc = start_pc;
 
   rv_write32(0x2000, -2);
   rv_write32(0x2004, -3);
-  while (1) {
-    if (rv_execute(&ctx)) {
+  while (1)
+  {
+    if (rv_execute(&ctx))
+    {
       printf("RV execution stopped\n");
       break;
     }
--- em/esw.c
+++ em/esw.c
@@ -10,3 +10,6 @@ void start() {
 }
 
 int foo(int a, int b) { return a + b; }
+int main() {
+	return 0;
+}
