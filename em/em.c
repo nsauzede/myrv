8324932e9943ea815a6a89278f2983217a6e5c4d
Nicolas Sauzede <nsauzede@laposte.net>
2021-09-17 01:22:10 +0200

    Basic ELF esw support

--- em/em.c
+++ em/em.c
@@ -1,7 +1,16 @@
+#include <fcntl.h>
 #include <malloc.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define HAVE_ELF
+#ifdef HAVE_ELF
+#include <libelf.h>
+#endif
 
 #include "rv.h"
 
@@ -13,6 +22,14 @@ uint32_t rv_read(void *dest, uint32_t addr, uint32_t size) {
   uint32_t ret = 0;
   if (dest && (addr >= mem_start) && (addr + size <= mem_start + mem_len)) {
     memcpy(dest, mem + addr - mem_start, size);
+  }
+  return ret;
+}
+
+uint32_t rv_write(const void *src, uint32_t addr, uint32_t size) {
+  uint32_t ret = 0;
+  if (src && (addr >= mem_start) && (addr + size <= mem_start + mem_len)) {
+    memcpy(mem + addr - mem_start, src, size);
   }
   return ret;
 }
@@ -33,6 +50,50 @@ int rv_write32(uint32_t addr, uint32_t val) {
   return 1;
 }
 
+#ifdef HAVE_ELF
+int elf_load(char *fname, uint32_t *entry) {
+  elf_version(EV_CURRENT);
+  int fd = open(fname, O_RDONLY, 0);
+  Elf *e = elf_begin(fd, ELF_C_READ, 0);
+  Elf_Kind ek = elf_kind(e);
+  if (ek != ELF_K_ELF) {
+    return 1;
+  }
+  // printf("elf kind=%s\n", ek == ELF_K_ELF ? "ELF" : "??");
+  if (!elf32_getehdr(e)) {
+    return 1;
+  }
+  Elf32_Ehdr *hdr = elf32_getehdr(e);
+  // printf("hdr32=%p\n", hdr);
+  // printf("entry=%08" PRIx32 "\n", hdr->e_entry);
+  if (entry) {
+    *entry = hdr->e_entry;
+  }
+  size_t nph;
+  elf_getphdrnum(e, &nph);
+  // printf("%d program headers\n", (int)nph);
+  Elf32_Phdr *ph = elf32_getphdr(e);
+  for (size_t i = 0; i < nph; i++) {
+    // printf(" of=%" PRIx32, ph[i].p_offset);
+    // printf(" pa=%" PRIx32, ph[i].p_paddr);
+    // printf(" va=%" PRIx32, ph[i].p_vaddr);
+    // printf(" fs=%" PRIx32, ph[i].p_filesz);
+    // printf(" ms=%" PRIx32, ph[i].p_memsz);
+    Elf_Data *d =
+        elf_getdata_rawchunk(e, ph[i].p_offset, ph[i].p_filesz, ELF_T_PHDR);
+    // printf(" %08" PRIx64, *(uint64_t *)d);
+    uint64_t *p = (uint64_t *)*(uint64_t *)d;
+    // printf(" %016" PRIx64, *p);
+    // printf("\n");
+    rv_write(p, ph[i].p_vaddr, ph[i].p_filesz);
+  }
+
+  elf_end(e);
+  close(fd);
+  return 0;
+}
+#endif
+
 int main(int argc, char *argv[]) {
   uint32_t start_pc = 0;
   uint32_t start_sp = 0x2000;
@@ -48,9 +109,18 @@ int main(int argc, char *argv[]) {
     }
   }
   mem = calloc(mem_len, 1);
-  FILE *in = fopen(fin, "rb");
-  fread(mem, mem_len, 1, in);
-  fclose(in);
+#ifdef HAVE_ELF
+  if (!elf_load(fin, &start_pc)) {
+    printf("loaded ELF\n");
+  } else {
+#endif
+    FILE *in = fopen(fin, "rb");
+    fread(mem, mem_len, 1, in);
+    fclose(in);
+    printf("loaded flat binary\n");
+#ifdef HAVE_ELF
+  }
+#endif
 
   rv_ctx ctx;
   rv_init(&ctx, rv_read, rv_read32, rv_write32);
