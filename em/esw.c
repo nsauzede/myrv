33901573d040c5469e8ca92412c20ef5f07e1d71
Nicolas Sauzede <nsauzede@laposte.net>
2021-09-19 16:01:41 +0200

    Early fstat support

--- em/esw.c
+++ em/esw.c
@@ -1,6 +1,10 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+
 #ifdef __riscv
 #define EBREAK asm volatile("ebreak")
 
@@ -29,6 +33,50 @@ int foo(int a, int b) {
 #define GREETING "Hello world\n"
 #endif
   write(1, GREETING, 12);
+  struct stat st;
+#ifdef __riscv
+  // return sizeof(st.st_dev); // 2
+  // return sizeof(st.st_ino);//2
+  // return sizeof(st.st_mode);//4
+  // return sizeof(st.st_nlink); // 2
+  // return sizeof(st.st_uid); // 2
+  // return sizeof(st.st_gid); // 2
+  // return sizeof(st.st_rdev); // 2
+  // return sizeof(st.st_size); // 4
+  // return sizeof(st.st_blksize); // 4
+  // return sizeof(st.st_blocks); // 4
+  // return sizeof(st.st_atim); // 16
+  // return sizeof(st.st_mtim); // 16
+  // return sizeof(st.st_ctim); // 16
+  // return sizeof(st.st_atim.tv_sec); // 8
+  fstat(0, &st);
+  // return st.st_dev;
+#else
+  // return sizeof(st.st_dev); // 8
+  // return sizeof(st.st_ino);//8
+  // return sizeof(st.st_mode);//4
+  // return sizeof(st.st_nlink); // 8
+  // return sizeof(st.st_atim); // 16
+  // return sizeof(st.st_atim.tv_sec); // 8
+  fstat(0, &st);
+#endif
+
+  // return sizeof(st.st_atim);//16
+  // return sizeof(st);//144
+#ifndef __riscv
+  printf("dev=%" PRIx32 "\n", (uint32_t)st.st_dev);
+  printf("ino=%" PRIx32 "\n", (uint32_t)st.st_ino);
+  printf("mode=%" PRIx32 "\n", (uint32_t)st.st_mode);
+  printf("nlink=%" PRIx32 "\n", (uint32_t)st.st_nlink);
+  printf("uid=%" PRIx32 "\n", (uint32_t)st.st_uid);
+  printf("gid=%" PRIx32 "\n", (uint32_t)st.st_gid);
+  printf("rdev=%" PRIx32 "\n", (uint32_t)st.st_rdev);
+  printf("size=%" PRIx32 "\n", (uint32_t)st.st_size);
+  printf("blksize=%" PRIx32 "\n", (uint32_t)st.st_blksize);
+  printf("blocks=%" PRIx32 "\n", (uint32_t)st.st_blocks);
+  printf("atim.sec=%" PRIx32 "\n", (uint32_t)st.st_atim.tv_sec);
+  printf("atim.nsec=%" PRIx32 "\n", (uint32_t)st.st_atim.tv_nsec);
+#endif
   // puts("hello puts\n");
   // printf("hello %s\n", "printf");
   return a + b;
--- em/rv.c
+++ em/rv.c
@@ -576,12 +576,64 @@ int rv_execute(rv_ctx *ctx) {
           break;
         }
         case 80: {
-          log_printf(1, "NOTIMP fstat a1=%" PRIx32 " \\\n", ctx->a1);
-          return 1;
+          typedef uint16_t rv_dev_t;
+          typedef uint16_t rv_ino_t;
+          typedef uint32_t rv_mode_t;
+          typedef uint16_t rv_nlink_t;
+          typedef uint16_t rv_uid_t;
+          typedef uint16_t rv_gid_t;
+          typedef uint32_t rv_off_t;
+          typedef uint32_t rv_blksize_t;
+          typedef uint32_t rv_blkcnt_t;
+          typedef struct rv_timespec {
+            uint64_t tv_sec, tv_nsec;
+          } rv_timespec_t;
+          // };
+          typedef struct rv_stat {
+            rv_dev_t st_dev;         /* ID of device containing file */
+            rv_ino_t st_ino;         /* Inode number */
+            rv_mode_t st_mode;       /* File type and mode */
+            rv_nlink_t st_nlink;     /* Number of hard links */
+            rv_uid_t st_uid;         /* User ID of owner */
+            rv_gid_t st_gid;         /* Group ID of owner */
+            rv_dev_t st_rdev;        /* Device ID (if special file) */
+            rv_off_t st_size;        /* Total size, in bytes */
+            rv_blksize_t st_blksize; /* Block size for filesystem I/O */
+            rv_blkcnt_t st_blocks;   /* Number of 512B blocks allocated */
+
+            /* Since Linux 2.6, the kernel supports nanosecond
+               precision for the following timestamp fields.
+               For the details before Linux 2.6, see NOTES. */
+
+            rv_timespec_t st_atim;      /* Time of last access */
+            struct rv_timespec st_mtim; /* Time of last modification */
+            struct rv_timespec st_ctim; /* Time of last status change */
+
+#define st_atime st_atim.tv_sec /* Backward compatibility */
+#define st_mtime st_mtim.tv_sec
+#define st_ctime st_ctim.tv_sec
+          } rv_stat_t;
+          rv_stat_t st;
+          memset(&st, 0, sizeof(st));
+          st.st_dev = 0x18;
+          st.st_ino = 0x4;
+          st.st_mode = 0x2190;
+          st.st_nlink = 0x1;
+          st.st_uid = 0x3e8;
+          st.st_gid = 0x5;
+          st.st_rdev = 0x8801;
+          st.st_size = 0x0;
+          st.st_blksize = 0x400;
+          st.st_blocks = 0x0;
+          ctx->write(&st, ctx->a1, sizeof(st));
+          log_printf(1, "fstat a1=%" PRIx32 "\n", ctx->a1);
+          printf("NOTIMP fstat a0=%" PRIx32 " a1=%" PRIx32 "\n", ctx->a0,
+                 ctx->a1);
+          // return 1;
           break;
         }
         case 93: {
-          log_printf(1, "exit a0=%" PRIx32 " \\\n", ctx->a0);
+          log_printf(1, "exit a0=%" PRIx32 "\n", ctx->a0);
           return 1;
           break;
         }
